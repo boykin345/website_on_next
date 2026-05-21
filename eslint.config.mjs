ef58835f4677d31c76f3c85da89710c445047f78
Mikhail K <juzzyboykin@gmail.com>
2026-05-21 22:40:44 +0500

    refactoring

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,9 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import prettier from "eslint-config-prettier/flat";
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  prettier,
-  {
-    rules: {
-      "react/no-unused-vars": "warn",
-    },
-  },
-  globalIgnores([
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  pluginReact.configs.flat.recommended,
 ]);
-
-export default eslintConfig;
